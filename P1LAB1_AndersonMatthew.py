3003f0327d02b6af8649d82ebb8e9ed1259dd212
Andersom8072 <71297237+Andersom8072@users.noreply.github.com>
2020-09-14 19:44:06 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_AndersonMatthew.py
@@ -0,0 +1,10 @@
+#basic zybooks assignment
+#09/14/2020
+#CTI-110 P1LAB1- Using Zybook Lab Basics
+#Matthew Anderson
+#
+
+userNum = int(input())
+userNumSquared= userNum * userNum  
+   
+print(userNumSquared)      
